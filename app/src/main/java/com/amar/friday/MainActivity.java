39769d0b7d24391f03ba5fd2e45b4c7ec0a36afb
Amar Kewalramani <gamingwithamar11224@gmail.com>
2026-05-31 13:17:04 +0530

    Initial Android setup and GitHub Action

---
+++ app/src/main/java/com/amar/friday/MainActivity.java
@@ -0,0 +1,12 @@
+package com.amar.friday;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // We will build the XML layout and attach it here next!
+    }
+}
